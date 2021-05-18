85935db89b534d50a25c1c31af616dfecc65a9a0
Valentin <valiopld@gmail.com>
2021-05-18 14:10:45 +0300

    install material ng

--- gui-web/src/app/app.module.ts
+++ gui-web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,9 +9,12 @@ import { CoreModule } from './@core/core.module';
 import { PagesModule } from './@pages/pages.module';
 import { SharedModule } from './@shared/shared.modules';
 
+import {MatCardModule} from '@angular/material/card'; 
+
 const NG_MODULES = [
   BrowserModule,
   AppRoutingModule,
+  BrowserAnimationsModule,
 ];
 
 const CUSTOM_MODULES = [
@@ -19,9 +23,14 @@ const CUSTOM_MODULES = [
   SharedModule,
 ];
 
+const MATERIAL_MODULES = [
+  MatCardModule,
+];
+
 const imports = [
   ...NG_MODULES,
   ...CUSTOM_MODULES,
+  ...MATERIAL_MODULES,
 ];
 
 const declarations = [
